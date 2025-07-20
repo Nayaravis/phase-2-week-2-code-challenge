b9fc4f87a03868c09ddb2736cc91207aeb95b651
Navis Ayara <navis.ochieng@student.moringaschool.com>
2025-07-20 18:37:59 +0300

    adds new goal doalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import { Plus } from 'lucide-react'
 import GoalsList from './components/GoalsList'
+import NewGoalDialog from './components/NewGoalDialog'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isNewClicked, updateIsNewClicked] = useState(false);
+  const [goalsList, updateGoalsList] = useState([]);    
+    useEffect(() => {
+        fetch("http://127.0.0.1:3000/goals")
+            .then(res => res.json())
+            .then(goals => {
+                updateGoalsList(goals);
+            })
+    }, [])
   return (
-    <div className='w-full h-full bg-gray-50'>
+    <div className='w-full h-full bg-gray-50 z-40'>
+      <NewGoalDialog isClicked={isNewClicked} goalsList={goalsList} updateIsClicked={updateIsNewClicked}/>
       <header className='w-full md:h-24 bg-white shadow p-6 flex justify-between items-center fixed'>
         <div>
           <h1 className='font-semibold text-3xl'>Financial Goals</h1>
           <p className='text-gray-500'>Track your progress towards financial independence</p>
         </div>
         <div>
-          <button className='bg-orange-600 rounded-full flex p-3.5 items-center gap-2.5 cursor-pointer'>
+          <button 
+            className='bg-orange-600 rounded-full flex p-3.5 items-center gap-2.5 cursor-pointer'
+            onClick={() => updateIsNewClicked(true)}
+            >
             <Plus color='white'/>
             <span className='text-white font-semibold text-lg'>New Goal</span>
           </button>
         </div>
       </header>
-      <div className='p-11 pt-36'>
-        <GoalsList />
+      <div className={`p-11 pt-36 ${isNewClicked ? "h-screen overflow-hidden" : ""}`}>
+        <GoalsList goalsList={goalsList}/>
       </div>
     </div>
   )
--- src/components/GoalsList.jsx
+++ src/components/GoalsList.jsx
@@ -1,17 +1,6 @@
-import { useEffect, useState } from "react";
-import Goal from "./Goal.tsx"
+import Goal from "./Goal.jsx"
 
-function GoalsList() {
-    const [goalsList, updateGoalsList] = useState([]);
-    
-    useEffect(() => {
-        fetch("http://127.0.0.1:3000/goals")
-            .then(res => res.json())
-            .then(goals => {
-                updateGoalsList(goals);
-            })
-    }, [])
-
+function GoalsList({ goalsList }) {
     return (
         <div className="my-2.5 flex flex-col gap-3.5">
             <span className="text-2xl font-semibold">Active Goals</span>
